b0f75c8706079c95504cc1e7ebe56dc2fba286eb
Ника <sexta.ggio@gmail.com>
2022-06-27 18:02:14 +0300

    отрисовка миниатюр

---
+++ js/miniatures.js
@@ -0,0 +1,18 @@
+import { newPosts } from './data';
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const picturesContainer = document.querySelector('.pictures');
+const picturesFragment = document.createDocumentFragment();
+const posts = newPosts();
+
+posts.forEach(({url, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  picturesFragment.append(pictureElement);
+} );
+
+picturesContainer.append(picturesFragment);
